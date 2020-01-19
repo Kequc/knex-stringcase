6c4092fbe4c17bfdb1c8643429241180e45e7346
kequc <v2xyvxkr.1646bpmi@mail.getinbox.io>
2020-01-19 01:22:22 +0100

    Slight code refactor

--- key-converter-factory.js
+++ key-converter-factory.js
@@ -1,17 +1,12 @@
 module.exports = keyConverterFactory;
 
 // String converter for keys of an object
-function keyConverterFactory (converter, recursiveStringcase = () => false) {
-    if (typeof recursiveStringcase !== 'function') {
-        throw new Error('Recursive stringcase must be a function');
-    }
-
+function keyConverterFactory (converter, recursive) {
     return function keyConverter (value, path, queryContext) {
-        if (!(value instanceof Object)) return value;
-        if (value instanceof Date) return value;
-        if (path !== 'root' && !recursiveStringcase(value, path, queryContext)) return value;
-
-        if (Array.isArray(value)) return value.map(item => keyConverter(item, path, queryContext));
+        if (isRaw(recursive, value, path, queryContext))
+            return value;
+        if (Array.isArray(value))
+            return value.map(item => keyConverter(item, path, queryContext));
 
         const output = {};
 
@@ -22,4 +17,13 @@ function keyConverterFactory (converter, recursiveStringcase = () => false) {
 
         return output;
     };
+}
+
+// Value remains unchanged
+function isRaw (recursive, value, path, queryContext) {
+    if (!(value instanceof Object)) return true;
+    if (value instanceof Date) return true;
+    if (path === 'root') return false;
+    if (typeof recursive !== 'function') return true;
+    return !recursive(value, path, queryContext);
 }
